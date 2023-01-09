3239262770f069f43f324833ee0d706f44b63c77
linyows <linyows@gmail.com>
2023-01-09 22:51:31 +0900

    use next/font

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,15 +2,25 @@ import '../styles/globals.css'
 import 'normalize.css/normalize.css'
 import type { AppProps } from 'next/app'
 import Head from 'next/head'
+import { Noto_Sans_JP, Noto_Serif_JP } from '@next/font/google'
 import Header from '../components/header'
 import Footer from '../components/footer'
 import GA from '../components/ga'
 import 'notionate/dist/styles/notionate.css'
 import 'notionate/dist/styles/notionate-dark.css'
 
+const notosans = Noto_Sans_JP({
+  weight: ['400', '900'],
+  subsets: ['japanese'],
+})
+const notoserif = Noto_Serif_JP({
+  weight: ['400', '900'],
+  subsets: ['japanese'],
+})
+
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <>
+    <main className={`${notosans.className} ${notoserif.className}`}>
       <Head>
         <link rel="alternate" type="application/rss+xml" href="/index.xml" title="RSS2.0" />
       </Head>
@@ -30,7 +40,7 @@ function MyApp({ Component, pageProps }: AppProps) {
           <Footer />
         </div>
       </div>
-    </>
+    </main>
   )
 }
 
