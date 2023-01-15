35d2432f31b5b256fec888e8654e7084a26f5312
linyows <linyows@gmail.com>
2023-01-15 17:21:09 +0900

    split localfont

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,67 +2,12 @@ import '../styles/globals.css'
 import 'normalize.css/normalize.css'
 import type { AppProps } from 'next/app'
 import Head from 'next/head'
-import localFont from '@next/font/local'
 import Header from '../components/header'
 import Footer from '../components/footer'
 import GA from '../components/ga'
 import 'notionate/dist/styles/notionate.css'
 import 'notionate/dist/styles/notionate-dark.css'
-
-const notosans = localFont({
-  src: [
-    {
-      path: './src/fonts/NotoSansJP-Black.woff2',
-      weight: '900',
-      style: 'normal',
-    },
-    {
-      path: './src/fonts/NotoSansJP-Regular.woff2',
-      weight: '400',
-      style: 'normal',
-    },
-  ],
-  fallback: [
-    'Noto Sans JP',
-    'Montserrat',
-    'system-ui',
-    '-apple-system',
-    'BlinkMacSystemFont',
-    'Segoe UI',
-    'Roboto',
-    'Helvetica Neue',
-    'Arial',
-    'Noto Sans',
-    'sans-serif',
-    'Apple Color Emoji',
-    'Segoe UI Emoji',
-    'Segoe UI Symbol',
-    'Noto Color Emoji',
-  ]
-})
-const notoserif = localFont({
-  src: [
-    {
-      path: './src/fonts/NotoSerifJP-Black.woff2',
-      weight: '900',
-      style: 'normal',
-    },
-    {
-      path: './src/fonts/NotoSerifJP-Regular.woff2',
-      weight: '400',
-      style: 'normal',
-    },
-  ],
-  fallback: [
-    'Noto Serif JP',
-    'Merriweather',
-    'Georgia',
-    'Cambria',
-    'Times New Roman',
-    'Times',
-    'serif',
-  ]
-})
+import { notosans, notoserif } from '../src/lib/fonts'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
---
+++ src/lib/fonts.ts
@@ -0,0 +1,57 @@
+import localFont from '@next/font/local'
+
+export const notosans = localFont({
+  src: [
+    {
+      path: '../fonts/NotoSansJP-Black.woff2',
+      weight: '900',
+      style: 'normal',
+    },
+    {
+      path: '../fonts/NotoSansJP-Regular.woff2',
+      weight: '400',
+      style: 'normal',
+    },
+  ],
+  fallback: [
+    'Noto Sans JP',
+    'Montserrat',
+    'system-ui',
+    '-apple-system',
+    'BlinkMacSystemFont',
+    'Segoe UI',
+    'Roboto',
+    'Helvetica Neue',
+    'Arial',
+    'Noto Sans',
+    'sans-serif',
+    'Apple Color Emoji',
+    'Segoe UI Emoji',
+    'Segoe UI Symbol',
+    'Noto Color Emoji',
+  ]
+})
+
+export const notoserif = localFont({
+  src: [
+    {
+      path: '../fonts/NotoSerifJP-Black.woff2',
+      weight: '900',
+      style: 'normal',
+    },
+    {
+      path: '../fonts/NotoSerifJP-Regular.woff2',
+      weight: '400',
+      style: 'normal',
+    },
+  ],
+  fallback: [
+    'Noto Serif JP',
+    'Merriweather',
+    'Georgia',
+    'Cambria',
+    'Times New Roman',
+    'Times',
+    'serif',
+  ]
+})
