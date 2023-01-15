ae09ef26f7938b942aa18fc58fbc5a22de73f733
linyows <linyows@gmail.com>
2023-01-15 14:32:26 +0900

    integrate html meta to hed.tsx

--- components/hed.tsx
+++ components/hed.tsx
@@ -4,16 +4,25 @@ import Head from 'next/head'
 type Props = {
   title?: string
   desc?: string
+  ogimage?: string
   children?: ReactNode
 }
 
-const Hed: FC<Props> = ({ title, desc, children }) => {
+const Hed: FC<Props> = ({ title, desc, ogimage, children }) => {
   const defaultTitle = 'Tomohisa Oda'
+  const defaultDesc = `Software Engineer, researcher, and writer, living in Fukuoka Japan.`
   const t = title === '' || title === undefined ? defaultTitle : `${title} - ${defaultTitle}`
+  const d = desc === '' || desc === undefined ? defaultDesc : desc
+  const url = 'https://tomohisaoda.com'
   return (
     <Head>
       <title>{t}</title>
-      {desc && <meta name="description" content={desc} />}
+      {desc && <meta name="description" content={d} />}
+      <meta property="og:site_name" content={t} />
+      {desc && <meta property="og:description" content={d} />}
+      {ogimage && <meta property="og:image" content={`${url}/${ogimage}`} />}
+      <meta property="og:url" content={url} />
+      <meta property="og:type" content="website" />
       {children}
     </Head>
   )
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -2,17 +2,9 @@ import Document, { Html, Head, Main, NextScript } from 'next/document'
 
 class MyDocument extends Document {
   render() {
-    const name = `Tomohisa Oda`
-    const desc = `Software Engineer, researcher, and writer, living in Fukuoka Japan.`
-    const url = `https://tomohisaoda.com`
     return (
       <Html lang="ja">
         <Head>
-          <meta property="og:url" content={url} />
-          <meta property="og:type" content="website" />
-          <meta property="og:description" content={desc} />
-          <meta property="og:site_name" content={name} />
-          <meta name="description" content={desc}></meta>
           <link rel="icon" href="/favicon.ico" />
         </Head>
         <body>
