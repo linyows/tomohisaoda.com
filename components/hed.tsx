8b959ff0b82dd57f7c1f22723832d1d70e0733c7
linyows <linyows@gmail.com>
2023-01-15 16:41:19 +0900

    specify og:url and canonical

--- components/hed.tsx
+++ components/hed.tsx
@@ -5,15 +5,17 @@ type Props = {
   title?: string
   desc?: string
   ogimage?: string
+  path?: string
   children?: ReactNode
 }
 
-const Hed: FC<Props> = ({ title, desc, ogimage, children }) => {
+const Hed: FC<Props> = ({ title, desc, ogimage, path, children }) => {
   const defaultTitle = 'Tomohisa Oda'
   const defaultDesc = `Software Engineer, researcher, and writer, living in Fukuoka Japan.`
+  const defaultUrl = `https://tomohisaoda.com`
   const t = title === '' || title === undefined ? defaultTitle : `${title} - ${defaultTitle}`
   const d = desc === '' || desc === undefined ? defaultDesc : desc
-  const url = 'https://tomohisaoda.com'
+  const url = path === '' || path === undefined ? defaultUrl : `${defaultUrl}${path}`
   return (
     <Head>
       <title>{t}</title>
@@ -21,8 +23,11 @@ const Hed: FC<Props> = ({ title, desc, ogimage, children }) => {
       <meta property="og:site_name" content={t} />
       {d && <meta property="og:description" content={d} />}
       {ogimage && <meta property="og:image" content={`${url}/${ogimage}`} />}
+      {ogimage && <meta property="og:image:width" content="1600" />}
+      {ogimage && <meta property="og:image:height" content="630" />}
       <meta property="og:url" content={url} />
       <meta property="og:type" content="website" />
+      <link rel="canonical" href={url} />
       {children}
     </Head>
   )
