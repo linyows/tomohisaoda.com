38b0ba18ea83f34ddd387077977d226b0a6c6c31
linyows <linyows@gmail.com>
2023-01-17 00:10:00 +0900

    update ogiimage

--- components/ogimage.tsx
+++ components/ogimage.tsx
@@ -5,40 +5,64 @@ type Props = {
   title: string
 }
 
+const divStyle: CSSProperties = {
+  display: 'flex',
+  color: '#fff',
+  backgroundColor: '#3d3d3d',
+  backgroundImage: 'url(https://raw.githubusercontent.com/linyows/tomohisaoda.com/main/public/ogibg.png)',
+  backgroundRepeat:  'no-repeat',
+  backgroundPosition: '0 0',
+  width: '100%',
+  height: '100%',
+  textAlign: 'center',
+  justifyContent: 'center',
+  alignItems: 'center',
+  flexDirection: 'column',
+  fontWeight: 'bold',
+}
+const iconStyle = {
+  marginRight: '24px',
+  borderRadius: 128,
+  verticalAlign: 'middle',
+}
+const nameStyle = {
+  marginTop: '20px',
+  marginRight: '30px',
+  fontSize: 50,
+}
+
+const titleStyle = {
+  fontSize: 50,
+  width: '80%',
+}
+const footerIconStyle = {
+  marginRight: '20px',
+  borderRadius: 128,
+  verticalAlign: 'middle',
+}
+const footerStyle = {
+  marginTop: '16px',
+  fontSize: 26,
+}
+const footerNameStyle = {
+  marginTop: '14px',
+}
+const footerUrlStyle = {
+  marginTop: '14px',
+  marginLeft: '10px',
+  fontWeight: 'normal',
+}
+
 const OgImage: FC<Props> = ({ title }) => {
   const name = `Tomohisa Oda`
   const iconSrc = `https://github.com/linyows.png`
-  const divStyle: CSSProperties = {
-    display: 'flex',
-    color: '#fff',
-    backgroundColor: '#3d3d3d',
-    backgroundImage: 'url(https://raw.githubusercontent.com/linyows/tomohisaoda.com/main/public/ogibg.png)',
-    backgroundRepeat:  'no-repeat',
-    backgroundPosition: '0 0',
-    width: '100%',
-    height: '100%',
-    textAlign: 'center',
-    justifyContent: 'center',
-    alignItems: 'center',
-    flexDirection: 'column',
-    fontWeight: 'bold',
-  }
-  const iconStyle = {
-    marginRight: '24px',
-    borderRadius: 128,
-    verticalAlign: 'middle',
-  }
-  const footerIconStyle = {
-    marginRight: '20px',
-    borderRadius: 128,
-    verticalAlign: 'middle',
-  }
+  const url = `https://tomohisaoda.com`
 
   if (title === '') {
     return (
       <div style={divStyle} >
         <img width="200" height="200" style={iconStyle} src={iconSrc} alt="icon" />
-        <p style={{ marginTop: '20px', marginRight: '30px', fontSize: 50 }} >
+        <p style={nameStyle} >
           {name}
         </p>
       </div>
@@ -47,17 +71,13 @@ const OgImage: FC<Props> = ({ title }) => {
 
   return (
     <div style={divStyle} >
-      <p style={{ fontSize: 40, width: '80%', }} >
+      <p style={titleStyle} >
         {title}
       </p>
-      <p style={{ marginTop: '16px', fontSize: 24, }} >
+      <p style={footerStyle} >
         <img width="70" height="70" style={footerIconStyle} src={iconSrc} alt="icon" />
-        <span style={{ marginTop: '14px' }} >
-          {name}
-        </span>
-        <span style={{ marginTop: '14px', marginLeft: '10px', fontWeight: 'normal' }} >
-          - https://tomohisaoda.com
-        </span>
+        <span style={footerNameStyle}>{name}</span>
+        <span style={footerUrlStyle}> - {url}</span>
       </p>
     </div>
   )
